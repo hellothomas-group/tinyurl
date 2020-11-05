7fc21c3cef96bec8f1bf5ab49ff73945f4e4fc2c
hellothomas <tyty2017@qq.com>
2020-11-05 09:38:06 +0800

    #comment 删除无用错误码

--- src/main/java/com/hellothomas/assignment/common/enums/ErrorCodeEnum.java
+++ src/main/java/com/hellothomas/assignment/common/enums/ErrorCodeEnum.java
@@ -2,16 +2,11 @@ package com.hellothomas.assignment.common.enums;
 
 public enum ErrorCodeEnum implements ICodeEnum {
     URL_FORMAT_ERROR("CODE001", "URL格式错误"),
-    FORWARD_ERROR("CODE002", "请求转发异常,异常为:{0}"),
-    EXTRACT_BODY_ERROR("CODE003", "解析body异常,异常为:{0}"),
-    GENERATE_URI_ERROR("CODE004", "重写URI异常,uri为:{0}"),
-    RESPONSE_PROCESS_ERROR("CODE005", "响应体处理失败,异常为:{0}"),
-    URL_NOT_EXIST("CODE006", "URL不存在"),
-    HTTPS_URL_CONNECTION_IS_EXCEPTED("CODE007", "An instance of HttpsURLConnection is expected"),
-    SSL_SOCKET_IS_EXCEPTED("CODE008", "An instance of SSLSocket is expected"),
-    GET_LOCAL_HOST_ERROR("CODE009", "获取本机host异常,异常为{0}"),
-    THREAD_SLEEP_ERROR("CODE010", "线程休眠异常,异常为{0}"),
-    GENERATE_SEQ_ERROR("CODE011", "生成序号失败"),
+    RESPONSE_PROCESS_ERROR("CODE002", "响应体处理失败,异常为:{0}"),
+    URL_NOT_EXIST("CODE003", "URL不存在"),
+    GET_LOCAL_HOST_ERROR("CODE004", "获取本机host异常,异常为{0}"),
+    THREAD_SLEEP_ERROR("CODE005", "线程休眠异常,异常为{0}"),
+    GENERATE_SEQ_ERROR("CODE006", "生成序号失败"),
     ;
 
     private String code;
